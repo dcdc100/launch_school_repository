f99f3d1fb6bf893f241df71ef18352d81a2a78bc
Derek Chang <dchang@gmail.com>
2016-01-20 22:42:14 +0000

    The Basics

---
+++ the_basics/exercise2.rb
@@ -0,0 +1,4 @@
+puts thousands = 5324 / 1000
+puts hundreds = 5324 % 1000 / 100
+puts tens = 5324 % 100 / 10
+puts ones = 5324 % 10
---
+++ the_basics/exercise3.rb
@@ -0,0 +1,12 @@
+movies = { jaws: 1975,
+          kill_bill: 2004,
+          frozen: 2013,
+          pearl_harbor: 2001,
+          raiders_of_the_lost_ark: 1981
+}
+
+puts movies [:jaws]
+puts movies [:kill_bill]
+puts movies [:frozen]
+puts movies [:pearl_harbor]
+puts movies [:raiders_of_the_lost_ark]
