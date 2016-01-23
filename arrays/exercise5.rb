84a0e73e86004f3de5ef6899ac064593f344256a
Derek Chang <dchang@gmail.com>
2016-01-23 22:46:08 +0000

    Add arrays directory

---
+++ arrays/exercise5.rb
@@ -0,0 +1,7 @@
+string = "Welcome to America!"
+a = string[6]     # e
+puts a
+b = string[11]    # A
+puts b
+c = string[19]    # nil
+puts c
---
+++ arrays/exercise7.rb
@@ -0,0 +1,9 @@
+a = [1, 2, 3, 4]
+new arr = []
+
+arr.each do |n|
+  new_arr << n + 2
+end
+
+p arr
+p new_arr
